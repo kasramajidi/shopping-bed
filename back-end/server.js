e067876c57a3316515236f647d613ced173ec1b0
kasra <kasramajdy81@gmail.com>
2025-02-17 11:07:38 +0330

    develop run server in port 5500

---
+++ back-end/server.js
@@ -0,0 +1,47 @@
+const app = require('./src/app')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+//* Load ENV
+
+const ProductionMode = process.env.MODE_ENV === "production"
+if (!ProductionMode) {
+    dotenv.config()
+}
+
+
+//* DataBase MONGODB connection
+
+async function ConnectioToDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected Successfully: ${mongoose.connection.host}`);
+    } catch (err) {
+        console.log("Error in DB connection ->", err);
+        process.exit(1)
+    }
+}
+
+//* run server
+
+function startServer(){
+    const port = process.env.PORT || 5500
+    app.listen(port, () => {
+        console.log(
+            `Server running in ${
+              ProductionMode ? "production" : "development"
+            } mode on port ${port}`
+          );
+    })
+}
+
+
+//* function run
+
+async function run() {
+    startServer();
+    await ConnectioToDB()
+}
+
+
+run()
