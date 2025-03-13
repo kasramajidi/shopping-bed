7f3896a3e25e4ea492eca02fd055e234fcded783
kasra <kasramajdy81@gmail.com>
2025-03-14 01:23:31 +0330

    responsive Navbar and Register

--- front-end/src/components/Layout/Navbar.jsx
+++ front-end/src/components/Layout/Navbar.jsx
@@ -1,64 +1,78 @@
 "use client";
-import { usePathname } from "next/navigation"
-import Link from "next/link"
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import { FaMoon } from "react-icons/fa6";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { useAuth } from "@/context/AuthContext";
+import { useState } from "react";
 
 export default function Navbar() {
-  const pathname = usePathname()
-  const { isAuthenticated } = useAuth()
+  const pathname = usePathname();
+  const { isAuthenticated } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className="bg-[#F0F6FF] py-3">
-      <div className="text-[#394E6A] max-w-5xl mx-auto flex items-center justify-between">
+      <div className="text-[#394E6A] max-w-5xl mx-auto flex items-center justify-between px-10">
+        {/* لوگو */}
         <Link
           href="/"
-          className="px-4 bg-[#057AFF] rounded-lg transition delay-200 text-[#DBE1FF] pb-2 text-3xl hover:bg-[#005CCC]"
+          className="lg:block hidden px-4 bg-[#057AFF] rounded-lg transition delay-200 text-[#DBE1FF] pb-2 text-3xl hover:bg-[#005CCC]"
         >
           c
         </Link>
-        <nav className="flex items-center text-[#394E6A] gap-5">
+
+        {/* دکمه منو برای موبایل */}
+        <button
+          className="lg:hidden text-xl"
+          onClick={toggleMenu}
+        >
+          {isOpen ? "X" : "☰"}
+        </button>
+
+        {/* منو */}
+        <nav
+          className={`lg:flex flex-col lg:flex-row lg:gap-5 lg:items-center absolute flex max-w-[250px] lg:static bg-[#F0F6FF] w-full top-20 left-10 lg:w-auto lg:bg-transparent transition-all ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
           {isAuthenticated ? (
             <>
               <Link
                 href="/"
-                className={`py-2 px-4 ${pathname === "/" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Home
               </Link>
               <Link
                 href="/about"
-                className={`py-2 px-4 ${pathname === "/about" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/about" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 About
               </Link>
               <Link
                 href="/products"
-                className={`py-2 px-4 ${pathname === "/products" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/products" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Products
               </Link>
               <Link
                 href="/cart"
-                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Cart
               </Link>
               <Link
                 href="/CheckOut"
-                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/CheckOut" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 CheckOut
               </Link>
               <Link
                 href="/Order"
-                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/Order" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Order
               </Link>
@@ -67,40 +81,40 @@ export default function Navbar() {
             <>
               <Link
                 href="/"
-                className={`py-2 px-4 ${pathname === "/" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Home
               </Link>
               <Link
                 href="/about"
-                className={`py-2 px-4 ${pathname === "/about" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/about" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 About
               </Link>
               <Link
                 href="/products"
-                className={`py-2 px-4 ${pathname === "/products" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/products" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Products
               </Link>
               <Link
                 href="/cart"
-                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"
-                  }`}
+                className={`py-2 px-4 ${pathname === "/cart" ? "bg-[#021431] text-[#C7C9D1] rounded-lg" : "bg-transparent text-[#394E6A]"}`}
               >
                 Cart
               </Link>
             </>
           )}
         </nav>
+
+        {/* آیکون‌ها */}
         <div className="flex items-center gap-5 text-xl">
           <FaMoon className="text-[rgb(57, 78, 106)] cursor-pointer" />
-          <Link href={"/cart"} className="relative">
+          <Link href="/cart" className="relative lg:block hidden">
             <RiShoppingCart2Line className="cursor-pointer" />
-            <span className="bg-[#0066CC] absolute px-1.5 text-white -top-4 rounded-lg -right-3 text-sm">0</span>
+            <span className="bg-[#0066CC] absolute px-1.5 text-white -top-4 rounded-lg -right-3 text-sm">
+              0
+            </span>
           </Link>
         </div>
       </div>
