fa9dccad1b0e9ce7aedc62170b7713bac1139eeb
kasra <kasramajdy81@gmail.com>
2025-03-14 00:56:09 +0330

    Add logout section

--- front-end/src/components/Layout/Register.jsx
+++ front-end/src/components/Layout/Register.jsx
@@ -1,12 +1,24 @@
 import Link from "next/link"
+import { useAuth } from "@/context/AuthContext"
+import Logout from "../Logout/Logout"
 
 export default function register() {
+  const {isAuthenticated} = useAuth()
   return (
     <section className="bg-[rgb(16,10,55)] py-2">
       <div className="max-w-5xl mx-auto flex items-center justify-end gap-x-6 text-[#C7C9D1]">
-          <Link href={"/pages/Login"} className="link link-hover text-xs sm:text-sm">Sign in / Guest</Link>
-          <Link href={"/pages/Signup"} className="link link-hover text-xs sm:text-sm">Create Account</Link>
-      </div>  
+        {isAuthenticated ? (
+          <>
+            <span className="text-xs">Hello, demo user</span>
+            <Logout/>
+          </>
+        ) : (
+          <>
+            <Link href={"/pages/Login"} className="link link-hover text-xs sm:text-sm">Sign in / Guest</Link>
+            <Link href={"/pages/Signup"} className="link link-hover text-xs sm:text-sm">Create Account</Link>
+          </>
+        )}
+      </div>
     </section>
   )
 }
---
+++ front-end/src/components/Logout/Logout.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useAuth } from "@/context/AuthContext";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+export default function Logout() {
+  const { setIsAuthenticated } = useAuth();
+  const router = useRouter();
+
+  const LogoutApi = async () => {
+    try {
+      await axios.post(
+        "http://localhost:5500/auth/logout"
+      );
+      localStorage.removeItem("token");
+      setIsAuthenticated(false);
+      router.push("/pages/Home");
+      toast.success("logout user", { position: "top-center" });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <button
+      onClick={LogoutApi}
+      className="bg-transparent px-2 text-xs rounded-sm cursor-pointer text-[rgb(5,122,255)] border border-[rgb(5,122,255)] hover:bg-[rgb(80,158,255)] hover:border-[rgb(80,158,255)] hover:text-white"
+    >
+      LOGOUT
+    </button>
+  );
+}
