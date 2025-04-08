9a357275134d41e528ba40d76fec70836c4acfb0
kasra <kasramajdy81@gmail.com>
2025-04-08 21:57:22 +0330

    Writing code for the shopping cart page and making the page responsive

--- front-end/src/app/Cart/page.tsx
+++ front-end/src/app/Cart/page.tsx
@@ -1,6 +1,90 @@
+"use client";
+import { JSX, useEffect, useReducer } from "react";
+import { cartReducer, initialState } from "../../components/cart/CartReducer";
+import TotalAmount from "@/components/cart/TotalAmount";
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
+import CartItems from "@/components/cart/CartItems";
 
-export default function page() {
+export type CartItemType = {
+  id: string;
+  title: string;
+  price: number;
+  amount: number;
+  image?: {
+    path: string;
+  };
+  company: string;
+  colors: string;
+};
+
+export type CartAction =
+  | { type: "INIT_CART"; payload: CartItemType[] }
+  | { type: "REMOVE"; payload: string }
+  | { type: "CHANGE_AMOUNT"; payload: { id: string; amount: number } };
+
+export default function CartPage(): JSX.Element {
+  const [cartItems, dispatch] = useReducer(
+    cartReducer as React.Reducer<CartItemType[], CartAction>,
+    initialState
+  );
+
+  const { isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    const stored = localStorage.getItem("cart");
+    if (stored) {
+      try {
+        const parsed: CartItemType[] = JSON.parse(stored);
+        dispatch({ type: "INIT_CART", payload: parsed });
+      } catch (err) {
+        console.error("Invalid JSON in cart:", err);
+      }
+    }
+  }, []);
+
+  const subtotal = cartItems.reduce(
+    (acc, item) => acc + item.price * item.amount,
+    0
+  );
+
+  const shipping = 500;
+  const totalAmount = cartItems.reduce((acc, item) => acc + item.amount, 0);
+
+  const tax = totalAmount * 1600;
+  const total = subtotal + shipping + tax;
+
   return (
-    <div className="text-[rgb(57,78,106)]">page</div>
-  )
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 py-18 flex flex-col text-[rgb(57,78,106)]">
+      {cartItems.length > 0 ? (
+        <>
+          <h1 className="text-2xl sm:text-3xl font-bold pb-6 border-b border-[rgb(229,231,235)]">
+            Shopping Cart
+          </h1>
+          <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
+            <CartItems cartItems={cartItems} dispatch={dispatch} />
+
+            <div className="flex flex-col mt-6 lg:mt-8 w-full lg:w-1/4 gap-4">
+              <TotalAmount
+                subtotal={subtotal}
+                shipping={shipping}
+                tax={tax}
+                total={total}
+              />
+
+              <button className="bg-blue-500  text-white font-semibold w-full sm:w-96 md:w-72 py-3 rounded-xl hover:bg-blue-600 transition-all duration-300">
+                <Link href={isAuthenticated ? "/Checkout" : "/Login"}>
+                  {isAuthenticated ? "Proceed to Checkout" : "Please Login"}
+                </Link>
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <h1 className="text-2xl sm:text-3xl font-bold pb-6 border-b border-[rgb(229,231,235)]">
+          Your cart is empty
+        </h1>
+      )}
+    </div>
+  );
 }
---
+++ front-end/src/components/cart/CartItems.tsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import { CartItemType, CartAction } from "@/app/Cart/page";
+import { JSX } from "react";
+
+type CartItemsProps = {
+  cartItems: CartItemType[];
+  dispatch: React.Dispatch<CartAction>;
+};
+
+export default function CartItems({
+  cartItems,
+  dispatch,
+}: CartItemsProps): JSX.Element {
+  return (
+    <div className="flex-1 space-y-6">
+      {cartItems.map((item) => (
+        <div
+          key={item.id}
+          className="flex flex-col  lg:flex-row items-start gap-6 pb-6 border-b border-[rgb(229,231,235)] p-4 mt-8 justify-between"
+        >
+          <Image
+            width={96}
+            height={96}
+            src={`http://localhost:5500${item.image?.path || ""}`}
+            alt={item.title}
+            className="w-32 h-32 rounded-lg object-cover"
+          />
+          <div className="flex flex-col gap-4">
+            <h2 className="font-semibold text-lg">{item.title}</h2>
+            <p className="text-sm text-gray-500">{item.company}</p>
+            <div className="flex items-center gap-2 mt-1">
+              <span>Color:</span>
+              <span
+                className="w-4 h-4 rounded-full"
+                style={{ backgroundColor: item.colors }}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <label htmlFor="amount" className="text-[rgb(57,78,106)] text-sm">
+              Amount
+            </label>
+            <select
+              className="select bg-white text-black pl-2 pr-5 cursor-pointer w-[50px] lg:h-6 sm:h-12 rounded-xl border border-[rgba(57,78,106,0.2)] focus:outline-2 focus:outline-[rgba(57,78,106,0.2)] focus:outline-offset-2 select-bordered"
+              id="amount"
+              value={item.amount}
+              onChange={(e) =>
+                dispatch({
+                  type: "CHANGE_AMOUNT",
+                  payload: {
+                    id: item.id,
+                    amount: Number(e.target.value),
+                  },
+                })
+              }
+            >
+              {[...Array(6)].map((_, i) => (
+                <option key={i} value={i + 1}>
+                  {i + 1}
+                </option>
+              ))}
+            </select>
+            <button
+              className="text-[rgb(5,122,255)] text-sm hover:underline cursor-pointer"
+              onClick={() => {
+                dispatch({ type: "REMOVE", payload: item.id });
+                localStorage.setItem(
+                  "cart",
+                  JSON.stringify(cartItems.filter((i) => i.id !== item.id))
+                );
+              }}
+            >
+              remove
+            </button>
+          </div>
+          <div className="font-semibold text-right">
+            ${(Number(item.price) / 100).toFixed(2)}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
