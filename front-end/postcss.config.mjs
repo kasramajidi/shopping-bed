bc2291dfba994d1c5638a24c2a86f4fbcd552806
kasra <kasramajdy81@gmail.com>
2025-04-14 20:39:39 +0330

    fix project for deploy

---
+++ front-end/postcss.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: {
+      '@tailwindcss/postcss': {},  // تغییر به @tailwindcss/postcss
+      autoprefixer: {},    
+    },
+  };
+  
+  export default config;
+  
